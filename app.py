54a4531cd4964fc52a18b42c8832b04d47e25e20
dolfius1er <charpentiernathan78@gmail.com>
2025-08-04 21:08:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -50,3 +50,10 @@ def categories():
 
 if __name__ == '__main__':
     app.run(debug=True)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
